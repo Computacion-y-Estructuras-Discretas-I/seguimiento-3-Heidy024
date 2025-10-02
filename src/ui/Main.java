c32614eea5bbeb96cfee19bcc9b4b0f5057c94cf
Heidy024 <heidytti@gmail.com>
2025-10-02 12:52:13 -0500

    Seguimiento 3

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 import structures.PilaGenerica;
 import structures.TablasHash;
@@ -40,8 +41,10 @@ public class Main {
                     String[] partes = lineaNumeros.trim().split("\\s+");
                     int[] numeros = new int[partes.length];
                     for (int i = 0; i < partes.length; i++) {
+
                         numeros[i] = Integer.parseInt(partes[i]);
                     }
+
 
                     encontrarParesConSuma(numeros, objetivo);
                     break;
@@ -64,19 +67,75 @@ public class Main {
      * @return true si esta balanceada, false si no
      */
     public boolean verificarBalanceo(String s) {
-        // TODO: completar 
-        return false;
+        
+        PilaGenerica<Character> pila = new PilaGenerica<>(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                pila.Push(c);
+             
+            } else if ((c == ')' || c == '}' || c == ']') && pila.getTop() > 0) {
+                
+                if (pila.getTop() == 0) {
+                    return false;
+            
+                } 
+                char apertura = pila.Pop();
+                if (!esParCorrecto(apertura, c)) {
+                    return false;
+                }
+     
+            }
+        }
+        return pila.getTop() == 0;
     }
 
+    public boolean esParCorrecto(char apertura, char cierre) {
+        return (apertura == '(' && cierre == ')') ||(apertura == '{' && cierre == '}') ||(apertura == '[' && cierre == ']');
+       
+    }
     /**
      * Encuentra y muestra todos los pares unicos de numeros que sumen objetivo usando TablasHash.
      * @param numeros arreglo de numeros enteros
      * @param objetivo suma objetivo
      */
     public void encontrarParesConSuma(int[] numeros, int objetivo) {
-        // TODO: completar
-    }
+  
+        try {
+        TablasHash tabla = new TablasHash(100);
+        LinkedList<String> paresEncontrados = new LinkedList<>();
+        
+        for(int i = 0; i < numeros.length; i++) {
+            tabla.insert(numeros[i], numeros[i]);
+        }
 
+        for(int i = 0; i < numeros.length; i++) {
+            int complemento = objetivo - numeros[i];
+            
+            if(tabla.search(complemento, complemento)) {
+    
+                int menor, mayor;
+                if (numeros[i] < complemento) {
+                    menor = numeros[i];
+                    mayor = complemento;
+                } else {
+                    menor = complemento;
+                    mayor = numeros[i];
+                }
+
+                String par = menor + ", " + mayor;
+
+                if(!paresEncontrados.contains(par)) {
+                    paresEncontrados.add(par);
+                    System.out.println("Par encontrado: (" + par + ")");
+                }
+            }
+        }
+        } catch (Exception e) {
+            
+        }
+        
+    }
     public static void main(String[] args) throws Exception {
         Main app = new Main();
         app.ejecutar();
